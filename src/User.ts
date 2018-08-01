129f501417a0860c933d099d0a3764427b19d9ce
Didier yufenyuy <didier.yufenyuy@synepsys.com>
2018-08-01 14:19:37 +0100

    minor update

--- src/User.ts
+++ src/User.ts
@@ -6,14 +6,12 @@ export class User {
     full_name: string;
     repo_count: number;
     followers: number;
-    repos?: Repo[];
 
     constructor(user_response: any) {
         this.login = user_response.login;
         this.full_name = user_response.name;
         this.repo_count = user_response.public_repos;
         this.followers = user_response.followers;
-        // this.repos = user_response.;
     }
 
 }
